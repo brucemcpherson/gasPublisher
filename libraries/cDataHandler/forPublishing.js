ff66ab92105b04a17bb4be85d9af1cc68590a2a9
Bruce McPherson <bruce@mcpher.com>
2015-06-09 14:59:29 +0100

    updated by GasGit automation

---
+++ libraries/cDataHandler/forPublishing.js
@@ -0,0 +1,13 @@
+
+function showMyScriptAppResource(s) {
+  try {
+    return ScriptApp.getResource(s);
+  }
+  catch (err) {
+    throw err + " getting script " + s;
+  }
+}
+
+function publicStuffDb() {
+  return ScriptDb.getMyDb();
+}
